70092bf45baba50d933804c63ec36e76a28763b3
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-15 22:54:43 +0200

    Make initial server configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,24 @@
-console.log('starting server');
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+const app = express();
+
+import morgan from 'morgan';
+import * as process from 'node:process';
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+const port = process.env.PORT || 5100;
+try {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
