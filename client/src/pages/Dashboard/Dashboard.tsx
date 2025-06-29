0fab5c7469254d2d0d3fb9bbcc080821b210ae78
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-29 12:51:52 +0200

    Implement dashboard accounts loading

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,24 @@
 import Wrapper from './Wrapper';
+import { useLoaderData } from 'react-router-dom';
+import type { Account } from '@shared/interfaces';
 
 const Dashboard = () => {
+  const accounts = useLoaderData();
+
   return (
     <Wrapper>
-      <div className="">Dashboard</div>
+      <div className="accounts-container">
+        {accounts.map((account: Account) => {
+          return (
+            <div className="account-card">
+              <h5 className="title">{account.name}</h5>
+              <p className="account-type">
+                {account.type.toLowerCase()} ({account.currencyCode})
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </Wrapper>
   );
 };
--- client/src/pages/Dashboard/Wrapper.ts
+++ client/src/pages/Dashboard/Wrapper.ts
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  .account-card {
+    border: 2px solid var(--border-light);
+    border-radius: var(--border-radius);
+    padding: 1rem;
+
+    .account-type {
+      text-transform: capitalize;
+      line-height: 1.5;
+    }
+  }
+`;
 
 export default Wrapper;
