2e9f1757fe99d1496100c50da3107199b3a5d0a1
Valentyn Zozulia <valikobird@protonmail.com>
2025-06-29 22:43:10 +0200

    CLIENT: Prepare not linked accounts for form select

--- client/src/pages/AddAccount/loader.ts
+++ client/src/pages/AddAccount/loader.ts
@@ -2,21 +2,25 @@ import cc from 'currency-codes';
 import type { AccountsResponse, GeneralApiResponse } from '../../interfaces';
 import { getNotLinkedAccounts } from '../../utils/customFetch';
 import { toast } from 'react-toastify';
-import type { Account } from '@shared/interfaces';
 import { ACCOUNT_TYPES } from '@shared/constants';
 
 const loader = async () => {
   const accountTypes: string[][] = Object.entries(ACCOUNT_TYPES);
   const currencies: string[][] = cc.data.map(({ code, currency }) => [code, `${code} - ${currency}`]);
 
-  let accounts: Account[];
+  let accounts: [string | null, string][];
   try {
     const response: AccountsResponse | GeneralApiResponse = await getNotLinkedAccounts();
     if ((response as GeneralApiResponse).msg) {
       toast.error((response as GeneralApiResponse).msg);
       accounts = [];
     } else {
-      accounts = (response as AccountsResponse).accounts;
+      accounts = (response as AccountsResponse).accounts.map((account) => [
+        account._id!,
+        `${account.name} (${account.currencyCode})`,
+      ]);
+
+      accounts.unshift([null, '']);
     }
   } catch {
     toast.error('Failed to load accounts');
