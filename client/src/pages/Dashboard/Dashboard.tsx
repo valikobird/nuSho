94c921191a9bea14cfe6f53f077894bb3833fcdb
Valentyn Zozulia <valikobird@protonmail.com>
2025-07-04 13:37:44 +0200

    CLIENT: Add account action icons

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,20 +1,28 @@
 import Wrapper from './Wrapper';
 import { useLoaderData } from 'react-router-dom';
 import type { Account } from '@shared/interfaces';
+import { MdOutlineInput } from 'react-icons/md';
+import { FaScaleBalanced } from 'react-icons/fa6';
 
 const Dashboard = () => {
-  const accounts = useLoaderData();
+  const accounts: Account[] = useLoaderData();
 
   return (
     <Wrapper>
       <div className="accounts-container">
         {accounts.map((account: Account) => {
           return (
-            <div className="account-card">
-              <h5 className="title">{account.name}</h5>
-              <p className="account-type">
-                {account.type.toLowerCase()} ({account.currencyCode})
-              </p>
+            <div className="account-card" key={account._id}>
+              <div className="info">
+                <h5 className="title">{account.name}</h5>
+                <p className="account-type">
+                  {account.type.toLowerCase()} ({account.currencyCode})
+                </p>
+              </div>
+              <div className="actions">
+                <MdOutlineInput />
+                <FaScaleBalanced />
+              </div>
             </div>
           );
         })}
--- client/src/pages/Dashboard/Wrapper.ts
+++ client/src/pages/Dashboard/Wrapper.ts
@@ -1,15 +1,30 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.section`
+  .accounts-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 1rem;
+  }
+
   .account-card {
     border: 2px solid var(--border-light);
     border-radius: var(--border-radius);
     padding: 1rem;
+    display: grid;
+    grid-template-columns: 1fr auto;
 
     .account-type {
       text-transform: capitalize;
       line-height: 1.5;
     }
+  }
+
+  .actions {
+    font-size: 1.2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
   }
 `;
 
