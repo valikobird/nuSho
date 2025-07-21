a886dcb6403ebbc66f85d00c6beeef5c7aedf0c0
Valentyn Zozulia <valikobird@protonmail.com>
2025-07-21 21:26:01 +0200

    Switch to esbuild

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,20 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    target: 'es2022',
+    tsconfigRaw: {
+      compilerOptions: {
+        target: 'es2022',
+        useDefineForClassFields: true,
+      },
+    },
+  },
+  build: {
+    target: 'es2022',
+    outDir: 'dist',
+    sourcemap: true,
+  },
   server: {
     proxy: {
       '/api': {
